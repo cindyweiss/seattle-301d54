aa1ec40fdbfe89959974ff06c687e831d70ee1ae
cindyweiss <weiss_cindy@yahoo.com>
2019-12-02 13:20:44 -0800

    finished out all the parts of the lab

--- class-00/lab-b/app.js
+++ class-00/lab-b/app.js
@@ -152,7 +152,7 @@ let joe = new Student('Joe', 'Schmoe', 100);
 
 // TODO: Uncomment the following line of code to see the output in the browser console
 // Note that the arrow function will cause this code to break!
-// console.log(joe);
+console.log(joe);
 
 // TODO: After viewing the previous console.log(), return the code to a working state.
 
@@ -164,7 +164,7 @@ Student.prototype.greeting = function () {
 
 // TODO: Uncomment the following line of code to see the output in the browser console
 // Note that the arrow function will cause this method to break!
-// console.log(joe.greeting());
+console.log(joe.greeting());
 
 // TODO: After viewing the previous console.log(), return the code to a working state.
 
@@ -175,7 +175,7 @@ Student.courseName = function () {
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(Student.courseName());
+console.log(Student.courseName());
 
 
 
@@ -186,17 +186,17 @@ Student.prototype.scope = function () {
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(joe.scope());
+console.log(joe.scope());
 
 Student.prototype.scopeArrow = () => console.log(this);
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(joe.scopeArrow());
+console.log(joe.scopeArrow());
 
 // TODO: Write a COMMENT below to answer the following questions.
 // 1. What is "this" when joe.scope() is invoked?
-//
+//it is equal to undefined on console log, and it is equal to the user defined in the function.
 // 2. What is "this" when joe.scopeArrow() is invoked?
-//
+//it is equal to undefined in console, and I am unsure of answer.
 // 3. Explain why "this" is different when an arrow function is used.
-//
+//because it causes the "this" to bubble up.
